e1b8234eb20e94e38a60632a4e1766eafdb3461f
eloise <elo.freydier@gmail.com>
2025-12-04 00:14:09 -0500

    tf idf done, result is in \data\tf_idf\tf_idf_results.txt

--- utils/tf_idf/tf_idf.py
+++ utils/tf_idf/tf_idf.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import re
 
-CSV_FILE_NAME = r'C:\Users\Default\Desktop\COMP370FinalProj\utils\tf_idf\manual_annotation_all_movie_posts.csv' 
+CSV_FILE_NAME = r'C:\Users\Default\Desktop\COMP370FinalProj\data\open_coding\manual_annotation_all_movie_posts.csv' 
 TITLE_COLUMN = 'title'
 TEXT_COLUMN = 'selftext'
 
@@ -17,6 +18,8 @@ def get_top_tf_idf_words(df: pd.DataFrame, text_col: str, topic_col: str, n_top_
     topic_results = {}
     unique_topics = df[topic_col].unique()
 
+    print(unique_topics)
+
     print(f"\n--- Starting TF-IDF Analysis for {len(unique_topics)} unique topics ---")
 
     for topic in unique_topics:
@@ -27,21 +30,27 @@ def get_top_tf_idf_words(df: pd.DataFrame, text_col: str, topic_col: str, n_top_
         #make custom stop words to include reddit lingo adn movie names and all + english disctionary stop words
         reddit_sw = ['op', 'redditor', 'post', 'comment', 'thread', 'subreddit', 'oc', 
                      'nsfw', 'spoiler', 'upvote','downvote', 'imho', 'tl;dr', 'edit',
-                     'tldr', 'fwiw','nsfw']
+                     'tldr', 'fwiw','nsfw', "https", "http", "www", "com", "reddit", "sub", "url",
+                     "img", "jpg", "gif", "png", "like", "just", "did"]
         
         movie_sw = ['movie', 'film', 'release', 'theater', 'streaming', 'watch',
             'trailer', 'review', 'discussion', 'premiere', 'box office',
             'cast', 'director', 'actor', 'actress', 'imdb', 'cinema',
-            'screening', 'dvd', 'blu-ray', 'netflix', 'hulu', 'disney',
-            'amazon prime', 'plot', 'scene', 'ending', 'spoiler']
+            'screening', 'plot', 'scene', 'ending', 'spoiler', "reviews", "movies", "scenes", "title"]
         
         movie_names = ["Kpop Demon Hunters", "Materialists", "Ballerina", "Bride Hard",
                        "F1 The Movie", "Jurassic World Rebirth", "How to Train Your Dragon",
                        "28 Years Later", "Elio", "M3GAN 2.0", "The Old Guard 2"]
         
-        combined_stop_words = ENGLISH_STOP_WORDS.union(reddit_sw + movie_sw + movie_names)
+        raw_stop_words = list(ENGLISH_STOP_WORDS) + reddit_sw + movie_sw + movie_names
+        combined_stop_words = tokenize_stopword_list(raw_stop_words)
 
-        vectorizer = TfidfVectorizer(stop_words=combined_stop_words, token_pattern=r'\b[a-zA-Z]{3,}\b', smooth_idf=True)
+
+        vectorizer = TfidfVectorizer(
+        stop_words=combined_stop_words,
+        token_pattern=r'\b[a-zA-Z]{3,}\b',
+        smooth_idf=True
+        )
         
         tfidf_matrix = vectorizer.fit_transform(topic_documents)
         feature_names = vectorizer.get_feature_names_out()
@@ -54,6 +63,13 @@ def get_top_tf_idf_words(df: pd.DataFrame, text_col: str, topic_col: str, n_top_
         topic_results[topic] = top_n_terms.to_dict()
 
     return topic_results
+
+def tokenize_stopword_list(words):
+    token_pattern = re.compile(r'\b[a-zA-Z]{3,}\b')
+    tokens = []
+    for w in words:
+        tokens.extend(token_pattern.findall(w.lower()))
+    return tokens
 
 
 df = pd.read_csv(CSV_FILE_NAME)
@@ -79,3 +95,8 @@ for topic, words_scores in top_words_by_topic.items():
         for word, score in sorted_scores
     ]
     print("* " + "\n* ".join(formatted_words))
+
+    #save to text file
+    with open(r"C:\Users\Default\Desktop\COMP370FinalProj\data\tf_idf\tf_idf_results.txt", "a", encoding="utf-8") as f:
+        f.write(f"\nTopic: **{topic}**\n")
+        f.write("* " + "\n* ".join(formatted_words) + "\n")
